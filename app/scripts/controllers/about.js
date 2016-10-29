c45f77f79b39d22d36f688c02608deecce15fd30
Georgi Naumov <gonaumov@gmail.com>
2016-10-29 16:58:45 +0300

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name prepositionsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the prepositionsApp
+ */
+angular.module('prepositionsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
