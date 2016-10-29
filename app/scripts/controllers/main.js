296973b94dc77baf402e44a3f67f7320f57fad7f
Georgi Naumov <gonaumov@gmail.com>
2016-10-29 18:29:44 +0300

    Save current progress.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,17 @@
  * Controller of the prepositionsApp
  */
 angular.module('prepositionsApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($http, $scope) {
+    $scope.prepositions = null;
+    $http.get('./data/result.json').then(function(result) {
+        $scope.prepositions = result.data;
+    });
+    $scope.selectHandler = function(allpreposition, currentPreposition) {
+      if(!allpreposition.description || !currentPreposition.description) {
+        return;
+      }
+      if(allpreposition.description === currentPreposition.description) {
+          currentPreposition.active = false;
+      }
+    }
   });
