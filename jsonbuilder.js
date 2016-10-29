cc13543edaf0d5953ddef5cf05ce742baa4b109b
Georgi Naumov <gonaumov@gmail.com>
2016-10-29 17:30:47 +0300

    Prepare json data

---
+++ jsonbuilder.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+fs.saveJSON = function(what) {
+    fs.writeFileSync('result.json', JSON.stringify(what, null, '  '), 'utf-8');
+};
+
+var result = fs.readdirSync('./app/images/').filter(function (item) {
+    return item != 'yeoman.png';
+}).map(function(item) {
+    return {
+        'imagePath' : './images' + item,
+        'description' : 'change me', 
+        'active': true 
+    }
+});
+
+fs.saveJSON(result);
+console.log("All done!");
